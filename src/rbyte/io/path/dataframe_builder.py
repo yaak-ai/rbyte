1e725a04d1d71fb01e7add93bd95c88190f96d96
Evgenii Gorchakov <evgorchakov@gmail.com>
2025-03-03 17:15:41 +0100

    feat: faster PathDataFrameBuilder
    - `Path.rglob` -> `os.scandir` (https://discuss.python.org/t/is-there-a-pathlib-equivalent-of-os-scandir/46626)
    - `parse` -> `pl.Series.str.extract_groups`

--- src/rbyte/io/path/dataframe_builder.py
+++ src/rbyte/io/path/dataframe_builder.py
@@ -1,17 +1,15 @@
 import os
-from collections.abc import Mapping
-from os import PathLike
-from pathlib import Path
+from collections.abc import Iterator, Mapping
+from functools import cached_property
 from typing import final
 
-import parse
 import polars as pl
 from polars._typing import PolarsDataType  # noqa: PLC2701
 from polars.datatypes import (
     DataType,  # pyright: ignore[reportUnusedImport]  # noqa: F401
     DataTypeClass,  # pyright: ignore[reportUnusedImport]  # noqa: F401
 )
-from pydantic import ConfigDict, validate_call
+from pydantic import ConfigDict, DirectoryPath, validate_call
 from structlog import get_logger
 from structlog.contextvars import bound_contextvars
 
@@ -21,40 +19,62 @@ logger = get_logger(__name__)
 type Fields = Mapping[str, PolarsDataType | None]
 
 
+def scantree(path: str) -> Iterator[str]:
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            yield from scantree(entry.path)
+        else:
+            yield entry.path
+
+
 @final
 class PathDataFrameBuilder:
     __name__ = __qualname__
 
     @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
-    def __init__(self, fields: Fields) -> None:
-        self._fields = fields
+    def __init__(self, *, fields: Fields, pattern: str) -> None:
+        if set(fields) != set(
+            pl.Series(dtype=pl.String).str.extract_groups(pattern).struct.fields
+        ):
+            logger.error(
+                msg := "field keys don't match pattern groups",
+                fields=fields,
+                pattern=pattern,
+            )
 
-    def __call__(self, path: PathLike[str]) -> pl.DataFrame:
-        with bound_contextvars(path=path):
-            result = self._build(path)
+            raise ValueError(msg)
+
+        self._fields = fields
+        self._pattern = pattern
+
+    @validate_call
+    def __call__(self, path: DirectoryPath) -> pl.DataFrame:
+        path_str = path.resolve().as_posix()
+        with bound_contextvars(path=path_str):
+            result = self._build(path_str)
             logger.debug("built dataframe", length=len(result))
 
             return result
 
-    def _build(self, path: PathLike[str]) -> pl.DataFrame:
-        parser = parse.compile(Path(path).resolve().as_posix())  # pyright: ignore[reportUnknownMemberType]
-        match parser.named_fields, parser.fixed_fields:  # pyright: ignore[reportUnknownMemberType]
-            case ([_, *_], []):  # pyright: ignore[reportUnknownVariableType]
-                pass
-
-            case (named_fields, fixed_fields):  # pyright: ignore[reportUnknownVariableType]
-                logger.error(
-                    msg := "parser not supported",
-                    named_fields=named_fields,
-                    fixed_fields=fixed_fields,
-                )
-                raise RuntimeError(msg)
-
-        parent = Path(
-            os.path.commonpath([path, parser._expression.replace("\\.", ".")])  # pyright: ignore[reportPrivateUsage]  # noqa: SLF001
+    def _build(self, path: str) -> pl.DataFrame:
+        return (
+            pl.LazyFrame({"path": scantree(path)})  # pyright: ignore[reportArgumentType]
+            .select(
+                pl.col("path")
+                .str.strip_prefix(path)
+                .str.strip_prefix("/")
+                .str.extract_groups(self._pattern)
+                .alias("groups")
+            )
+            .unnest("groups")
+            .drop_nulls()
+            .select(self._fields)
+            .cast(self._schema, strict=True)  # pyright: ignore[reportArgumentType]
+            .collect()
         )
-        paths = map(Path.as_posix, parent.rglob("*"))
-        parsed = map(parser.parse, paths)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType]
-        data = [x.named for x in parsed if isinstance(x, parse.Result)]  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
 
-        return pl.DataFrame(data=data, schema=self._fields)  # pyright: ignore[reportUnknownArgumentType]
+    @cached_property
+    def _schema(self) -> Mapping[str, PolarsDataType]:
+        return {
+            name: dtype for name, dtype in self._fields.items() if dtype is not None
+        }
