718624bc7c03802be10e7370b839c727ee97c377
Evgenii Gorchakov <evgorchakov@gmail.com>
2025-03-14 12:16:11 +0100

    feat: generalized RerunLogger (#41)

--- src/rbyte/viz/loggers/rerun_logger.py
+++ src/rbyte/viz/loggers/rerun_logger.py
@@ -1,31 +1,29 @@
-from collections.abc import Iterable, Mapping, Sequence
+from collections import defaultdict
+from collections.abc import Callable, Mapping, Sequence
 from functools import cache, cached_property
 from math import prod
-from typing import Annotated, Any, Protocol, Self, cast, override, runtime_checkable
+from typing import (
+    Annotated,
+    Any,
+    ClassVar,
+    Literal,
+    Protocol,
+    cast,
+    override,
+    runtime_checkable,
+)
 
-import numpy as np
-import numpy.typing as npt
+import more_itertools as mit
 import rerun as rr
-import rerun.blueprint as rrb
-from pydantic import (
-    BeforeValidator,
-    Field,
-    ImportString,
-    RootModel,
-    model_validator,
-    validate_call,
-)
-from pydantic.types import AnyType
-from rerun._baseclasses import (
-    Archetype,  # noqa: PLC2701
-    ComponentColumn,
-)
+import torch
+from hydra.utils import get_method, instantiate
+from pydantic import BeforeValidator, ConfigDict, Field, RootModel, validate_call
 from rerun._send_columns import TimeColumnLike as _TimeColumnLike  # noqa: PLC2701
 from structlog import get_logger
 from structlog.contextvars import bound_contextvars
+from tensordict import TensorClass, TensorDict
 
-from rbyte.batch import Batch
-from rbyte.config import BaseModel
+from rbyte.config.base import BaseModel, HydraConfig
 
 from .base import Logger
 
@@ -36,168 +34,204 @@ logger = get_logger(__name__)
 class TimeColumnLike(_TimeColumnLike, Protocol): ...
 
 
-class ImageFormat(BaseModel):
-    pixel_format: (
-        Annotated[rr.PixelFormat, BeforeValidator(rr.PixelFormat.auto)] | None
-    ) = None
+class IndexSchemaItem(HydraConfig[TimeColumnLike]):
+    model_config: ClassVar[ConfigDict] = ConfigDict(extra="forbid")
 
-    color_model: (
-        Annotated[rr.ColorModel, BeforeValidator(rr.ColorModel.auto)] | None
-    ) = None
-
-    @model_validator(mode="after")
-    def validate_model(self: Self) -> Self:
-        if not (bool(self.pixel_format) ^ bool(self.color_model)):
-            msg = "pixel_format xor color_model must be specified"
-            raise ValueError(msg)
-
-        return self
+    times: str | tuple[str, ...]
 
 
-RerunImportString = Annotated[
-    ImportString[AnyType],
-    BeforeValidator(
-        lambda x: f"rerun.{x}"
-        if isinstance(x, str) and not x.startswith("rerun.")
-        else x
-    ),
-]
+class MethodHydraConfig[T](BaseModel):
+    model_config: ClassVar[ConfigDict] = ConfigDict(extra="allow")
+
+    target: Annotated[Callable[..., T], BeforeValidator(get_method)] = Field(
+        alias="_target_"
+    )
+
+    def instantiate(self, **kwargs: object) -> T:
+        return instantiate(self.model_dump(by_alias=True), **kwargs)
 
 
-TimeConfig = RerunImportString[type[TimeColumnLike]]
+class AsComponentsConfig(MethodHydraConfig[rr.AsComponents]): ...
 
-ComponentConfig = (
-    RerunImportString[type[Archetype]]
-    | Annotated[
-        Mapping[RerunImportString[type[rr.Image | rr.DepthImage]], ImageFormat],
-        Field(max_length=1),
+
+class ComponentColumnListConfig(MethodHydraConfig[rr.ComponentColumnList]):
+    __pydantic_extra__: dict[str, str | tuple[str, ...]]  # pyright: ignore[reportIncompatibleVariableOverride]
+
+
+class StaticSchemaItem(BaseModel):
+    static: Literal[True]
+    entity: AsComponentsConfig
+
+
+class Schema(
+    RootModel[
+        Mapping[
+            str,
+            IndexSchemaItem
+            | StaticSchemaItem
+            | ComponentColumnListConfig
+            | Sequence[StaticSchemaItem | ComponentColumnListConfig],
+        ]
     ]
-)
-
-
-class Schema(RootModel[Mapping[str, TimeConfig | ComponentConfig]]):
+):
     @cached_property
-    def indexes(self) -> Mapping[str, TimeColumnLike]:
-        return {k: v for k, v in self.root.items() if isinstance(v, TimeColumnLike)}
+    def indexes(self) -> Mapping[str, IndexSchemaItem]:
+        return {k: v for k, v in self.root.items() if isinstance(v, IndexSchemaItem)}
 
     @cached_property
-    def columns(
-        self,
-    ) -> Mapping[
-        str, type[Archetype] | Mapping[type[rr.Image | rr.DepthImage], ImageFormat]
-    ]:
-        return {k: v for k, v in self.root.items() if not isinstance(v, TimeColumnLike)}  # pyright: ignore[reportReturnType]
+    def static(self) -> Mapping[str, Sequence[StaticSchemaItem]]:
+        result: Mapping[str, list[StaticSchemaItem]] = defaultdict(list)
+
+        for k, v in self.root.items():
+            match v:
+                case StaticSchemaItem():
+                    result[k].append(v)
+
+                case Sequence():
+                    result[k].extend(x for x in v if isinstance(x, StaticSchemaItem))
+
+                case _:
+                    pass
+
+        return {k: v for k, v in result.items() if v}
+
+    @cached_property
+    def columns(self) -> Mapping[str, Sequence[ComponentColumnListConfig]]:
+        result: Mapping[str, list[ComponentColumnListConfig]] = defaultdict(list)
+
+        for k, v in self.root.items():
+            match v:
+                case ComponentColumnListConfig():
+                    result[k].append(v)
+
+                case Sequence():
+                    result[k].extend(
+                        x for x in v if isinstance(x, ComponentColumnListConfig)
+                    )
+
+                case _:
+                    pass
+
+        return {k: v for k, v in result.items() if v}
 
 
-class RerunLogger(Logger[Batch]):
-    @validate_call(config=BaseModel.model_config)
+class RerunLogger(Logger[TensorDict | TensorClass]):
+    @validate_call(config=ConfigDict(arbitrary_types_allowed=True))
     def __init__(
         self,
         *,
+        application_id: str | tuple[str, ...],
         schema: Schema,
         spawn: bool = True,
-        blueprint: rrb.BlueprintLike | None = None,
     ) -> None:
         super().__init__()
 
+        self._application_id: str | tuple[str, ...] = application_id
         self._schema: Schema = schema
         self._spawn: bool = spawn
-        self._blueprint: rrb.BlueprintLike | None = blueprint
 
     @cache  # noqa: B019
     def _get_recording(self, application_id: str) -> rr.RecordingStream:
         recording = rr.new_recording(
             application_id, spawn=self._spawn, make_default=True
         )
-        if self._blueprint is not None:
-            rr.send_blueprint(self._blueprint, recording=recording)
+
+        for path, items in self._schema.static.items():
+            rr.log(
+                path,
+                *(item.entity.instantiate() for item in items),
+                static=True,
+                recording=recording,
+            )
 
         return recording
 
     @classmethod
     def _build_columns(
-        cls,
-        array: npt.NDArray[Any],
-        schema: type[Archetype] | Mapping[type[rr.Image | rr.DepthImage], ImageFormat],
-    ) -> Iterable[ComponentColumn]:
-        match schema:
-            case rr.Scalar:
-                return rr.Scalar.columns(scalar=array)
+        cls, config: ComponentColumnListConfig, data: TensorDict
+    ) -> rr.ComponentColumnList:
+        kwargs = TensorDict({k: data[v] for k, v in config.__pydantic_extra__.items()})
 
-            case rr.Points3D:
-                match shape := array.shape:
-                    case (3,):
-                        return rr.Points3D.columns(positions=array)
-
-                    case (*batch_dims, n, 3):
-                        return rr.Points3D.columns(
-                            positions=array.reshape(-1, 3)
-                        ).partition([n] * prod(batch_dims))
-
-                    case _:
-                        logger.debug("not implemented", shape=shape)
-
-                        raise NotImplementedError
-
-            case rr.Tensor:
-                return rr.Tensor.columns(data=array)
-
-            case {rr.Image: image_format} | {rr.DepthImage: image_format}:
-                with bound_contextvars(image_format=image_format, shape=array.shape):
-                    match (
-                        image_format.pixel_format,
-                        image_format.color_model,
-                        array.shape,
-                    ):
-                        case None, rr.ColorModel(), (*batch_dims, height, width, _):
+        with bound_contextvars(target=config.target):
+            match cast(Any, config.target):
+                case rr.Image.columns | rr.DepthImage.columns:
+                    match (tensor := kwargs[(key := "buffer")]).shape:
+                        case (*batch_dims, _h, _w, 3):
                             pass
 
-                        case rr.PixelFormat.NV12, None, (*batch_dims, dim, width):
-                            height = int(dim / 1.5)
+                        case (*batch_dims, 3, _h, _w):
+                            tensor = tensor.permute(*range(len(batch_dims)), -2, -1, -3)
 
-                        case _:
-                            logger.error("not implemented")
+                        case (*batch_dims, _d, _w):
+                            pass
 
-                            raise NotImplementedError
+                        case shape:
+                            logger.error(
+                                (msg := "shape not supported"), key=key, shape=shape
+                            )
+                            raise NotImplementedError(msg)
 
-                format = rr.components.ImageFormat(
-                    height=height,
-                    width=width,
-                    pixel_format=image_format.pixel_format,
-                    color_model=image_format.color_model,
-                    channel_datatype=rr.ChannelDatatype.from_np_dtype(array.dtype),
-                )
+                    kwargs[key] = tensor.reshape(prod(batch_dims), -1).view(torch.uint8)
 
-                batch_dim = prod(batch_dims)
+                    return config.instantiate(**kwargs.cpu().numpy())  # pyright: ignore[reportUnknownMemberType]
 
-                return rr.Image.columns(
-                    buffer=array.reshape(batch_dim, -1).view(np.uint8),
-                    format=[format] * batch_dim,
-                )
+                case rr.Points3D.columns:
+                    match (tensor := kwargs[key := "positions"]).shape:
+                        case (3,):
+                            return config.instantiate(**kwargs.cpu().numpy())  # pyright: ignore[reportUnknownMemberType]
 
-            case _:
-                logger.error("not implemented")
+                        case (*batch_dims, n, 3):
+                            kwargs[key] = tensor.view(-1, 3)
+                            return config.instantiate(**kwargs.cpu().numpy()).partition(  # pyright: ignore[reportUnknownMemberType]
+                                [n] * prod(batch_dims)
+                            )
 
-                raise NotImplementedError
+                        case shape:
+                            logger.error(
+                                (msg := "shape not supported"), key=key, shape=shape
+                            )
+                            raise NotImplementedError(msg)
+
+                case _:
+                    return config.instantiate(**kwargs.cpu().numpy())  # pyright: ignore[reportUnknownMemberType]
 
     @override
-    def log(self, batch_idx: int, batch: Batch) -> None:
-        for i, sample in enumerate(batch.data):  # pyright: ignore[reportArgumentType, reportUnknownVariableType]
-            with self._get_recording(batch.meta.input_id[i]):  # pyright: ignore[reportUnknownArgumentType, reportOptionalSubscript, reportUnknownMemberType, reportOptionalMemberAccess]
-                indexes: Sequence[TimeColumnLike] = [
-                    index(
-                        timeline=timeline,
-                        times=np.atleast_1d(sample.get(timeline).numpy()),  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType, reportCallIssue]
+    def log(self, data: TensorDict | TensorClass) -> None:
+        data = data.to_tensordict()
+
+        match application_id := self._application_id:
+            case str():
+                with self._get_recording(application_id):
+                    self._log(data)
+
+            case tuple():
+                for application_id_elem, data_elem in zip(  # pyright: ignore[reportUnknownVariableType]
+                    map(str, data[application_id]), data, strict=True
+                ):
+                    with self._get_recording(application_id_elem):
+                        self._log(data_elem)  # pyright: ignore[reportUnknownArgumentType]
+
+    def _log(self, data: TensorDict) -> None:
+        indexes = [
+            config.instantiate(
+                timeline=timeline,
+                times=torch.atleast_1d(data[config.times].flatten()).cpu().numpy(),  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType]
+            )
+            for timeline, config in self._schema.indexes.items()
+        ]
+
+        for entity_path, configs in self._schema.columns.items():
+            with bound_contextvars(entity_path=entity_path):
+                columns = [self._build_columns(config, data) for config in configs]
+
+                try:
+                    rr.send_columns(
+                        entity_path=entity_path,
+                        indexes=indexes,
+                        columns=mit.flatten(columns),
+                        strict=True,
                     )
-                    for timeline, index in self._schema.indexes.items()
-                ]
+                except Exception:
+                    logger.exception("rr.send_columns failed")
 
-                for entity_path, schema in self._schema.columns.items():
-                    with bound_contextvars(path=entity_path, schema=schema):
-                        array = cast(
-                            npt.NDArray[Any],
-                            sample.get(entity_path).cpu().numpy(),  # pyright: ignore[reportUnknownMemberType]
-                        )
-
-                        columns = self._build_columns(array, schema)
-                        rr.send_columns(entity_path, indexes, columns, strict=True)
+                    raise
