8e70750405df94d167f2bf641704af7fe4574dd7
Nikita Koritskii <koritcky@gmail.com>
2025-03-17 15:25:12 +0100

    rm extra deps

--- src/rbyte/io/_json/dataframe_builder.py
+++ src/rbyte/io/_json/dataframe_builder.py
@@ -1,13 +1,10 @@
 import json
-import json
 from collections.abc import Mapping
 from os import PathLike
 from pathlib import Path
 from typing import ClassVar, final
-from typing import ClassVar, final
 
 import polars as pl
-from optree import PyTree, PyTreeAccessor, tree_map, tree_map_with_accessor
 from optree import PyTree, PyTreeAccessor, tree_map, tree_map_with_accessor
 from polars._typing import PolarsDataType  # noqa: PLC2701
 from polars.datatypes import (
@@ -15,11 +12,11 @@ from polars.datatypes import (
     DataTypeClass,  # pyright: ignore[reportUnusedImport]  # noqa: F401
 )
 from pydantic import ConfigDict, RootModel, validate_call
-from pydantic import ConfigDict, RootModel, validate_call
 from structlog import get_logger
 from structlog.contextvars import bound_contextvars
 
 logger = get_logger(__name__)
+
 
 class Schema(RootModel[Mapping[str, PolarsDataType | None]]):
     model_config: ClassVar[ConfigDict] = ConfigDict(arbitrary_types_allowed=True)
@@ -44,17 +41,6 @@ class JsonDataFrameBuilder:
             return result
 
     def _build(self, path: PathLike[str]) -> PyTree[pl.DataFrame]:
-        with Path(path).open(encoding=None) as fp:
-            data = json.load(fp)
-
-        def json_normalize(accessor: PyTreeAccessor, schema: Schema) -> pl.DataFrame:
-            return pl.json_normalize(accessor(data), schema=schema.root)  # pyright: ignore[reportArgumentType]
-
-        return tree_map_with_accessor(
-            json_normalize,
-            self._fields,  # pyright: ignore[reportArgumentType]
-            is_leaf=lambda x: isinstance(x, Schema),
-        )
         with Path(path).open(encoding=None) as fp:
             data = json.load(fp)
 
